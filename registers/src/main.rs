b92be151b5543a129ea9a900a3968ea64fd6f997
Koushik Alapati <143474630+akoushik2k@users.noreply.github.com>
2025-09-22 22:56:56 +0000

    day 4

--- registers/src/main.rs
+++ registers/src/main.rs
@@ -4,8 +4,6 @@
 use core::panic::PanicInfo;
 
 use cortex_m::asm;
-use cortex_m::peripheral::syst::SystClkSource;
-use cortex_m::peripheral::Peripherals;
 use cortex_m_rt::entry;
 use cortex_m_rt::exception;
 use cortex_m_semihosting::hprintln;
@@ -15,6 +13,15 @@ fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
+// System tick - control and status register
+const SYST_CSR: u32 = 0xE000E010;
+
+// System tick - reload value register
+const SYST_RVR: u32 = 0xE000E014;
+
+// System tick - current value register
+const SYST_CVR: u32 = 0xE000E018;
+
 // CPU frequency (12.5 MHz by default)
 const CPU_FREQ: u32 = 12_500_000;
 
@@ -22,13 +29,17 @@ const CPU_FREQ: u32 = 12_500_000;
 fn main() -> ! {
     hprintln!("Starting program!");
 
-    let peripherals = Peripherals::take().unwrap();
-    let mut systick = peripherals.SYST;
-    systick.enable_interrupt();
-    systick.set_clock_source(SystClkSource::Core);
-    systick.set_reload(CPU_FREQ);
-    systick.clear_current();
-    systick.enable_counter();
+    let sleep_dur = CPU_FREQ; // 1 second
+
+    unsafe {
+        *(SYST_RVR as *mut u32) = sleep_dur; // Set reload register
+
+        //clear the current value register
+        *(SYST_CVR as *mut u32) = 0;
+
+        //Enable the times
+        *(SYST_CSR as *mut u32) = 0b111; // Enable SysTick, its interrupt, and select processor clock
+    }
 
     loop {
         asm::wfi();
