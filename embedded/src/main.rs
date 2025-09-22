654cea3b387d7808329def86c65576908f349fc1
Koushik Alapati <143474630+akoushik2k@users.noreply.github.com>
2025-09-22 22:37:33 +0000

    day 4

--- embedded/src/main.rs
+++ embedded/src/main.rs
@@ -3,7 +3,10 @@
 
 use core::panic::PanicInfo;
 
-use cortex_m_rt::entry;
+use cortex_m::peripheral::syst::SystClkSource;
+use cortex_m::{asm, peripheral};
+use cortex_m::peripheral::Peripherals;
+use cortex_m_rt::{entry, exception};
 use cortex_m_semihosting::hprintln;
 
 #[panic_handler]
@@ -11,8 +14,25 @@ fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
+//CPU Frequency (12.5Mhz default)
+const CPU_FREQ: u32 = 12_500_000;
+
 #[entry]
 fn main() -> ! {
     hprintln!("Starting program!");
-    loop {}
+    let peripherals = Peripherals::take().unwrap();
+    let mut systick = peripherals.SYST;
+    systick.enable_interrupt();
+    systick.set_clock_source(SystClkSource::Core);
+    systick.set_reload(CPU_FREQ);
+    systick.clear_current();
+    systick.enable_counter();
+    loop {
+      asm::wfi()
+    }
 }
+
+#[exception]
+fn SysTick() {
+    hprintln!("SysTick");
+} 
